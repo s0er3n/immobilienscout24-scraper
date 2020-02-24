19e1947e12c3b8c27af34ec01cb597109a1e81a2
Sören Michaels <soeren.michaels@gmail.com>
2020-02-24 01:18:08 +0100

    automatic fill out added

--- filloutform.py
+++ filloutform.py
@@ -1,5 +1,3 @@
-import requests
-from requests_html import HTMLSession
 import sqlite3
 from sqlite3 import Error
 from selenium import webdriver
@@ -7,10 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
 
-opts = Options()
-opts.add_argument(
-    "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"
-)
+
 
 
 def create_connection(db_file):
@@ -28,7 +23,7 @@ def create_connection(db_file):
     return conn
 
 
-def select_all_flats(conn):
+def select_all_flats_without_useability_value(conn):
     """
     Query all rows in the tasks table
     :param conn: the Connection object
@@ -44,26 +39,42 @@ def select_all_flats(conn):
 
     return rows
 
+def select_useable_flats(conn):
+    """
+    returns rows with useable flats
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Wohnung WHERE useable is 1")
 
-def main():
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    return rows
+
+
+def find_useable_flats():
     database = r"wohnungen.db"
 
     # create a database connection
     conn = create_connection(database)
     cur = conn.cursor()
-    driver = webdriver.Chrome("/home/soeren/immobilienscout24-scraper/chromedriver")
+    driver = webdriver.Chrome()
     with conn:
-        session = HTMLSession()
-        for row in select_all_flats(conn):
+        for row in select_all_flats_without_useability_value(conn):
             url = (
                 f"https://www.immobilienscout24.de/expose/{row[0]}#/basicContact/email"
             )
             while True:
                 try:
                     driver.get(url)
+                    time.sleep(1)
                     break
                 except:
                     print("Error")
+                    driver.quit()
+                    driver = webdriver.Chrome()
                     time.sleep(5)
 
 
@@ -77,6 +88,21 @@ def main():
             conn.commit()
 
 
+def fill_out_form(driver, url, gender, message, firstname, lastname, email, phonenumber, streetname, housenumber, postcode, city):
+    profile = [message, firstname, lastname, email, phonenumber, streetname, housenumber, postcode, city]
+    driver.get(url)
+    time.sleep(2)
+    ids = ["contactForm-Message","contactForm-firstName","contactForm-lastName","contactForm-emailAddress", "contactForm-phoneNumber","contactForm-street","contactForm-houseNumber", "contactForm-postcode","contactForm-city"]
+    for id, text in zip(ids, profile):
+        driver.find_element_by_id(id).clear()
+        driver.find_element_by_id(id).send_keys(text)
+    driver.find_element_by_xpath(f"//select[@id='contactForm-salutation']/option[text()='{gender}']").click()
+    time.sleep(5)
+
 if __name__ == "__main__":
-    main()
+    
+    find_useable_flats()
+    driver = webdriver.Chrome()
+    fill_out_form(driver) # needs profiles
+
 
